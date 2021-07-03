d5393803bd453b74b21059e55a20c05b98788eef
Anwesh43 <anweshthecool0@gmail.com>
2021-07-03 22:49:59 +0530

    adding BDDNode class

--- biballdropdownview/src/main/java/com/example/biballdropdownview/BallDropDownView.kt
+++ biballdropdownview/src/main/java/com/example/biballdropdownview/BallDropDownView.kt
@@ -117,4 +117,46 @@ class BallDropDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDDNode(var i : Int, val state : State = State()) {
+
+        private var next : BDDNode? = null
+        private var prev : BDDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BDDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDDView(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDDNode {
+            var curr : BDDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
